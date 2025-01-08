2cd361ec32fb51b4afff762ea6b5f81f115ccd30
DripTrace <rpalm@driptrace.io>
2025-01-08 17:30:12 -0600

    temp rem auth

--- front/src/app/modify/page.tsx
+++ front/src/app/modify/page.tsx
@@ -2,8 +2,8 @@ import GlassAuthenticator from "@/components/modify/GlassAuthenticator";
 import { VideoEditor } from "@/components/modify/VideoEditor";
 
 const ModifyPage = () => {
-	// return <VideoEditor />;
-	return <div className="text-white">t</div>;
+	return <VideoEditor />;
+	// return <div className="text-white">t</div>;
 	// return <GlassAuthenticator />;
 };
 
